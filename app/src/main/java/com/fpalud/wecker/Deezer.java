a9a23cc74cc0fde8d5be64dcc34e79d09cb65bf3
Sykless <rakouett-du-56@hotmail.fr>
2018-12-04 16:58:54 +0100

    Read tracks inside user playlists + play them

--- app/src/main/java/com/fpalud/wecker/Deezer.java
+++ app/src/main/java/com/fpalud/wecker/Deezer.java
@@ -3,14 +3,28 @@ package com.fpalud.wecker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.deezer.sdk.model.Permissions;
+import com.deezer.sdk.model.Playlist;
+import com.deezer.sdk.model.Track;
 import com.deezer.sdk.network.connect.DeezerConnect;
 import com.deezer.sdk.network.connect.SessionStore;
-import com.deezer.sdk.network.connect.event.DialogListener;
+import com.deezer.sdk.network.request.DeezerRequest;
+import com.deezer.sdk.network.request.DeezerRequestFactory;
+import com.deezer.sdk.network.request.event.JsonRequestListener;
+import com.deezer.sdk.network.request.event.RequestListener;
+import com.deezer.sdk.player.AlbumPlayer;
+import com.deezer.sdk.player.TrackPlayer;
+import com.deezer.sdk.player.networkcheck.WifiAndMobileNetworkStateChecker;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Deezer extends AppCompatActivity
 {
     DeezerConnect deezerConnect;
+    ArrayList<Long> playlistIdList = new ArrayList<>();
+    long favoritePlaylistId = 0;
+
+    Track test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -18,39 +32,78 @@ public class Deezer extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.deezer_layout);
 
-        String applicationID = "315304";
-        deezerConnect = new DeezerConnect(this, applicationID);
+        deezerConnect = new DeezerConnect(this, "315304");
 
         // restore any saved session
         SessionStore sessionStore = new SessionStore();
 
-        if (sessionStore.restore(deezerConnect, this))
+        if (!sessionStore.restore(deezerConnect, this))
         {
-            System.out.println("Already logged in !");
+            System.out.println("ERROR : A traiter");
         }
-        else
+
+        RequestListener listener = new JsonRequestListener()
         {
-            String[] permissions = new String[] {
-                    Permissions.BASIC_ACCESS,
-                    Permissions.MANAGE_LIBRARY,
-                    Permissions.LISTENING_HISTORY };
+            public void onResult(Object result, Object requestId)
+            {
+                List<Playlist> playlistList = (List<Playlist>) result;
 
-            // The listener for authentication events
-            DialogListener listener = new DialogListener() {
-                public void onComplete(Bundle values)
+                for (Playlist playlist : playlistList)
                 {
-                    SessionStore sessionStore = new SessionStore();
-                    sessionStore.save(deezerConnect, getApplicationContext());
+                    if (playlist.isLovedTracks())
+                    {
+                        RequestListener playlistListener = new JsonRequestListener()
+                        {
+                            public void onResult(Object result, Object requestId)
+                            {
+                                List<Track> trackList = (List<Track>) result;
 
-                    System.out.println("Logged in !");
+                                for (Track track : trackList)
+                                {
+                                    playMusic(track.getId());
+                                    break;
+                                }
+                            }
+
+                            public void onUnparsedResult(String requestResponse, Object requestId) {}
+                            public void onException(Exception e, Object requestId) {}
+                        };
+
+                        DeezerRequest request = DeezerRequestFactory.requestPlaylistTracks(playlist.getId());
+                        deezerConnect.requestAsync(request, playlistListener);
+
+                        break;
+                    }
                 }
+            }
 
-                public void onCancel() {System.out.println("Canceled !");}
-                public void onException(Exception e) {System.out.println("Exception !");}
-            };
+            public void onUnparsedResult(String requestResponse, Object requestId) {}
+            public void onException(Exception e, Object requestId) {}
+        };
 
-            // Launches the authentication process
-            deezerConnect.authorize(this, permissions, listener);
+        DeezerRequest request = DeezerRequestFactory.requestCurrentUserPlaylists();
+        deezerConnect.requestAsync(request, listener);
+    }
+
+    void playMusic(long id)
+    {
+        System.out.println("Launch");
+
+        try
+        {
+            System.out.println("Try");
+
+            TrackPlayer trackPlayer = new TrackPlayer(getApplication(), deezerConnect, new WifiAndMobileNetworkStateChecker());
+            trackPlayer.playTrack(id);
+
+            System.out.println("Stopped");
+
+            // trackPlayer.stop();
+            // trackPlayer.release();
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
         }
     }
 }
--- app/src/main/java/com/fpalud/wecker/Home.java
+++ app/src/main/java/com/fpalud/wecker/Home.java
@@ -8,10 +8,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.deezer.sdk.model.Permissions;
+import com.deezer.sdk.network.connect.DeezerConnect;
+import com.deezer.sdk.network.connect.SessionStore;
+import com.deezer.sdk.network.connect.event.DialogListener;
+
 import java.util.Calendar;
 
 public class Home extends AppCompatActivity
 {
+    DeezerConnect deezerConnect;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -19,10 +26,47 @@ public class Home extends AppCompatActivity
         setContentView(R.layout.home_layout);
     }
 
-    public void goToDeezer(View view)
+    public void goToDeezer()
     {
         Intent intent = new Intent(this, Deezer.class);
         this.startActivity(intent);
+    }
+
+    public void launchDeezer(View view)
+    {
+        deezerConnect = new DeezerConnect(this, "315304");
+
+        // restore any saved session
+        SessionStore sessionStore = new SessionStore();
+
+        if (sessionStore.restore(deezerConnect, this))
+        {
+            goToDeezer();
+        }
+        else
+        {
+            String[] permissions = new String[] {
+                    Permissions.BASIC_ACCESS,
+                    Permissions.MANAGE_LIBRARY,
+                    Permissions.LISTENING_HISTORY };
+
+            // The listener for authentication events
+            DialogListener listener = new DialogListener() {
+                public void onComplete(Bundle values)
+                {
+                    SessionStore sessionStore = new SessionStore();
+                    sessionStore.save(deezerConnect, getApplicationContext());
+
+                    goToDeezer();
+                }
+
+                public void onCancel() {System.out.println("Canceled !");}
+                public void onException(Exception e) {System.out.println("Exception !");}
+            };
+
+            // Launches the authentication process
+            deezerConnect.authorize(this, permissions, listener);
+        }
     }
 
     public void setAlarm()
